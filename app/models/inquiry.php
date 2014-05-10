eda7b56a8446d3a59329cbb9d23b1397bc2d8774
jm-factorin <jm.factorin@klab.com>
2014-05-10 10:29:45 +0800

    Added inquiry function

---
+++ app/models/inquiry.php
@@ -0,0 +1,10 @@
+<?php
+
+class Inquiry extends AppModel
+{
+    public static function send($phone, $email, $message)
+    {
+        $con = DB::conn();
+        $con->insert('inquiry', array('phone' => $phone, 'email' => $email, 'message' => $message));
+    }
+}
